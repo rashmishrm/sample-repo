b6ac21d54689c7b48b103444ac8c1c0f62aedee2
Rashmi Sharma <rashmishrm74@gmail.com>
2017-05-03 17:41:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import json
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello from Dockerized Flask App!! changed file"
+    return "Hello from Dockerized Flask App!! changed file ddddd"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=5000)
