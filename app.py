9d4950332ff81ff30b0616cf2ebad051bf5bac8b
rashmishrm <rashmishrm74@gmail.com>
2017-05-03 17:28:41 -0700

    Sample repo changes

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-import psutil
-import operator
+from flask import Flask
+from flask import request
+from flask import json
 
-from operator import attrgetter
-from collections import namedtuple
-from itertools import groupby
 
+<<<<<<< HEAD
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello from Dockerized Flask App!!"
+
+=======
 #This Function prints all tcp connections grouped by pid,status.
 def print_connections():
         
@@ -21,22 +26,8 @@ def print_connections():
         for key, group in groupby(sorted_connections, key=attrgetter('pid')):
             grouped_map[key] = [e for e in group]
             pid_count_map[key]=len(grouped_map[key])
-
-        #sorting pid's by counts in descending order
-        sorted_by_count = sorted(pid_count_map, key=pid_count_map.get, reverse=True)
-
-        print '"pid","laddr","raddr","status"'
-
-        #Print connections per sorted pid based on count
-        for pid in sorted_by_count:
-            connections_per_pid = grouped_map[pid]
-            for x in connections_per_pid:
-                    #Converting each tuple from list to socket_conn namedTuple for better accessibilty
-                    p = socket_conn(*x)
-                    if(len(p.laddr)!=0 and len(p.raddr)!=0):
-                        laddr = "%s@%s" %(p.laddr[0],p.laddr[1]);
-                        raddr = "%s@%s" %(p.raddr[0],p.raddr[1]);
-                        print    '"%s","%s","%s","%s"' %(p.pid,laddr,raddr,p.status)
+>>>>>>> origin/master
 
 
-print_connections()
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=5000)
