a2b9f4c1dd6281ebda8cc8361c1ab2824df93678
Rashmi Sharma <rashmishrm74@gmail.com>
2017-05-10 17:18:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import json
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "asdsadasdasdasdasds. 45345435345345"
+    return "Changed!! finally working"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=5005)
